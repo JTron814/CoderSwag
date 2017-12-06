f1c50d3ab2694c285633d5c7e4e6f95487e5b43b
JTron814 <joshbors14@gmail.com>
2017-12-05 23:24:56 -0600

    Added list view, adapter, and list view layout.

--- app/src/main/java/com/example/joshb/coderswag/Model/Category.kt
+++ app/src/main/java/com/example/joshb/coderswag/Model/Category.kt
@@ -3,4 +3,8 @@ package com.example.joshb.coderswag.Model
 /**
  * Created by joshb on 12/5/2017.
  */
-class Category (val title: String, val image: String)
+class Category (val title: String, val image: String) {
+    override fun toString(): String {
+        return title
+    }
+}
